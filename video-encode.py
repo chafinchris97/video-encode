060f50e0aba1019e5e57efa72fe746d471ef99c1
chafinchris97 <chafin.chris.97@gmail.com>
2023-05-02 21:36:03 -0700

    add parsing arguments

--- video-encode.py
+++ video-encode.py
@@ -14,5 +14,32 @@
 #
 # dependencies: handbrakecli, ffprobe, ffmpeg, dovi_tool, mkvmerge, mkvpropedit, mkvextract
 
+import argparse
+
 if __name__ == '__main__':
-    print('hello world')
+    parser = argparse.ArgumentParser(
+        prog='Video Encode',
+        description='Creates high quality encodes in h265 10 bit, using Apple\'s VideoToolBox.'
+    )
+
+    parser.add_argument('file_name',
+                        type=str,
+                        help='directory for blu ray rip to encode')
+    parser.add_argument('--target',
+                        type=int,
+                        metavar='bitrate',
+                        default=4000,
+                        help='choose a bitrate to target (DEFAULT: 1080p=4000, 2160p=12000)')
+    parser.add_argument('--quality',
+                        type=int,
+                        help='choose a cq number to use to encode (overrides finding cq)')
+    parser.add_argument('--burn_subtitle',
+                        type=str,
+                        metavar='auto|none|TRACK',
+                        default='auto',
+                        help='pick which subtitle track to burn in (DEFAULT: auto - burns only image based subtitles that are forced or if main audio is foriegn)')
+    parser.add_argument('--crop',
+                        action='store_true',
+                        help='choose to auto crop (DEFAULT: no crop)')
+    
+    arguments = parser.parse_args()
